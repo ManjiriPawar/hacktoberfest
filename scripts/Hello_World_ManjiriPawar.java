00bba3431c2c343357bcc4e5b21411c6c6b40e79
Manjiri Pawar <118203511+ManjiriPawar@users.noreply.github.com>
2024-10-02 11:11:26 +0530

    Create Hello_World_ManjiriPawar.java

---
+++ scripts/Hello_World_ManjiriPawar.java
@@ -0,0 +1,11 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Manjiri Pawar
+  GITHUB: https://github.com/ManjiriPawar
+*/
+
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
